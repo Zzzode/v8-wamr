aadb5803161615ada2aae5fbb647950ec9bc430a
zode <zhangdi.zode@bytedance.com>
2023-08-21 12:02:03 +0800

    build: add auto generated lib folder

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,6 @@
 const filepath = __dirname;
-const rootpath = `${filepath}/..`
-const { add } = require(`${rootpath}/build/Release/addon.node`);
+const rootpath = `${filepath}/..`;
+
+const { add } = require(`${rootpath}/build/libs/addon.node`);
+
 console.log(add(2, 3));
